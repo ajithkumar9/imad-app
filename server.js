f635f47e652c2e9f94548585d892058c08ff0e3a
ajithkumar9 <31153250+ajithkumar9@users.noreply.github.com>
2017-08-23 06:51:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,8 +6,24 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
+  res.send('Article one will be served here');
+});
+
+app.get('/', function (req, res) {
+  res.send('Article two will be served here');
+});
+
+
+app.get('/', function (req, res) {
+  res.send('Article three will be served here');
+});
+
+
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
